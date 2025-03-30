6c6a2a7fb58794966a7f18b87184aaa0d3fb0431
Bradley Hoza <bradley.hoza@providence.org>
2025-03-30 08:59:50 -0700

    oops fix large-print-crosswords.sh arg handling

--- large-print-crosswords.sh
+++ large-print-crosswords.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 set -xue
 
-NYT_USER=${NYT_USER:-${1:-}}
-NYT_PASS=${NYT_PASS:-${2:-}}
+if [[ $# -lt 1 ]]; then
+    echo "usage: $0 <puzzle> <date> [<nyt_user>] [<nyt_pass>]"
+    echo "  puzzle: list of puzzles to fetch, separated by spaces (e.g. \"uni nyt\")"
+    echo "  date: list of dates to fetch, separated by spaces (e.g. \"2025-02-14 2025-02-15 2025-02-16\")"
+    echo "  nyt_user: New York Times username (optional)"
+    echo "  nyt_pass: New York Times password (optional)"
+    exit 1
+fi
+if [[ $# -gt 4 ]]; then
+    echo "too many arguments"
+    exit 1
+fi
+
+# replace NYT_USER and NYT_PASS with the values passed as arguments, if they are present
+if [[ -n "${3:-}" ]]; then
+    NYT_USER=${3:-}
+fi
+if [[ -n "${4:-}" ]]; then
+    NYT_PASS=${4:-}
+fi
 
 image_name="large-print-crosswords"
 image_tag="latest"
@@ -12,14 +30,13 @@ docker build . -f dockerfile -t "$image_name:$image_tag"
 
 echo ""
 echo "running..."
-args=$(printf "'%s' " "$@")
 docker run --rm --name lpc \
     -v "/$PWD/src":/src-live \
     -v "/$PWD/data":/data-live \
-    -e NYT_USER=$NYT_USER \
-    -e NYT_PASS=$NYT_PASS \
+    -e "NYT_USER=${NYT_USER:-}" \
+    -e "NYT_PASS=${NYT_PASS:-}" \
     "$image_name:$image_tag" \
-    //bin/bash -c "\/src-live/lpc-process.sh $args"
+    //bin/bash -c "\/src-live/lpc-process.sh \"$1\" \"$2\""
 
 echo ""
 echo "run completed successfully."
